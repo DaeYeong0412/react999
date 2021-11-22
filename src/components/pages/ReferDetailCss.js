9115df5e9156f689fe86f32a5fb9d8b8bb7924ed
DaeYeong <thdeodud0412@gmail.com>
2021-11-22 19:57:52 +0900

    zcxv
    xzcvcxzv

--- src/components/pages/ReferDetailCss.js
+++ src/components/pages/ReferDetailCss.js
@@ -1,34 +1,35 @@
-import React from "react";
-import Header from "../layouts/Header";
-import Contents from "../layouts/Contents";
-import Footer from "../layouts/Footer";
-import { Link } from "react-router-dom";
-import ContInfo from "../layouts/ContInfo";
+import React from 'react';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+import Contents from '../layouts/Contents';
+import { Link } from 'react-router-dom';
+import ContInfo from '../layouts/ContInfo';
 
-class ReferDetail extends React.Component {
-    componentDidMount() {
-        const { location, history} = this. props;
-        console.log(location);
+// function ReferDetail(props) {
+//     console.log(props);
+//     return <span>ddd</span>
+// }
 
-        if(location.state == undefined) {
-            history.push("/reference")
+class ReferDetailCss extends React.Component {
+    componentDidMount(){
+        const { location, history } = this.props;
+        if( location.state === undefined ){
+            history.push('/reference');
         }
     }
 
     render(){
-        const {location} = this.props;
-        const {title,desc1,desc2,element,tag,version,use,definition,view} = this.props.location.state.cssRefer;
-        return (
-            location.state
-            ? (
+        const { location } = this.props;
+        if( location.state ){
+            return (
                 <div>
-                    <Header></Header>
+                    <Header />
                     <Contents>
-                    <section id="referCont">
+                        <section id="referCont">
                             <div className="container">
                                 <div className="refer__detail pt200">
-                                    <h3>{title}</h3>
-                                    <p>{desc2}</p>
+                                    <h3>{location.state.title}</h3>
+                                    <p>{location.state.desc2}</p>
                                     <table>
                                         <thead>
                                             <tr>
@@ -38,42 +39,43 @@ class ReferDetail extends React.Component {
                                         </thead>
                                         <tbody>
                                             <tr>
-                                                <th>기본 값</th>
-                                                <td>{element}</td>
+                                                <th>기본값</th>
+                                                <td>{location.state.default1}</td>
                                             </tr>
                                             <tr>
                                                 <th>적용</th>
-                                                <td>{tag}</td>
+                                                <td>{location.state.apply}</td>
                                             </tr>
                                             <tr>
                                                 <th>버전</th>
-                                                <td>{version}</td>
+                                                <td>{location.state.version}</td>
                                             </tr>
                                             <tr>
                                                 <th>사용성</th>
-                                                <td>{use}</td>
+                                                <td>{location.state.use}</td>
                                             </tr>
                                         </tbody>
                                     </table>
 
                                     <h4>정의(Definition)</h4>
                                     <ul>
-                                        {definition.map((definition) => (
-                                            <li key={definition.toString()}>{definition}</li>
+                                        {location.state.definition.map((defer) => (
+                                            <li key={defer.toString()}>{defer}</li>
                                         ))}
                                     </ul>
                                     <Link className="list-btn" to="/reference">목록보기</Link>
                                 </div>
                             </div>
                         </section>
-                        <ContInfo></ContInfo>
+                        <ContInfo />
                     </Contents>
-                    <Footer></Footer> 
+                    <Footer />
                 </div>
             )
-            :<span></span>
-        )
+        } else {
+            return null;
+        }
     }
 }
 
-export default ReferDetail
+export default ReferDetailCss;
